02376ee949a65dc59801665681a2b06bc0fc8667
zhangjunlong <1589759543@qq.com>
2021-04-23 20:29:05 +0800

    wuhu

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import VueRouter from "vue-router"
+import Vue from 'vue'
+const Home = () =>
+    import ('../view/home/home.vue')
+const category = () =>
+    import ('../view/category/category.vue')
+const cart = () =>
+    import ('../view/cart/cart.vue')
+const profile = () =>
+    import ('../view/me/me.vue')
+Vue.use(VueRouter)
+const routers = [{
+    path: '',
+    redirect: '/home'
+}, {
+    path: '/home',
+    component: Home
+}, {
+    path: '/category',
+    component: category
+}, {
+    path: '/cart',
+    component: cart
+}, {
+    path: '/me',
+    component: profile
+}]
+const router = new VueRouter({
+    routers,
+    mode: 'history'
+})
+export default {
+    router
+}
